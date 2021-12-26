345b9e34caf99012f68e90ff437b4dd34514b386
Yiğit Şık <yigit6958@gmail.com>
2021-12-26 23:26:04 +0300

    fetch: add pitch bend effect

--- midi.h
+++ midi.h
@@ -2,11 +2,12 @@
 #define NOTE_OFF 0x80
 #define CONTROLLER 0xB0
 #define INSTRUMENT 0xC0
+#define PITCH 0xE0
 
 const byte majorScale[] = {0,2,4,5,7,9,10,11};
 
 
-void noteOn(int cmd, int pitch, int velocity) {
+void send_midi(int cmd, int pitch, int velocity) {
   Serial.write(cmd);
   Serial.write(pitch);
   Serial.write(velocity);
--- midiglove.ino
+++ midiglove.ino
@@ -2,7 +2,10 @@
 #include <MPU6050_tockn.h>
 #include <Wire.h>
 
-const int flexPin = A0; //pin A0 to read analog input
+const byte flexPin = A0; //pin A0 to read analog input
+const byte joystickXPin = A4;
+const byte joystickYpin = A5;
+const byte joystickButtonPin = 2;
 
 int value;
 byte majorScaleLength = majorScaleLength = sizeof(majorScale) / sizeof(majorScale[0]);;
@@ -20,58 +23,61 @@ float yawGyro;
 float yawAccel;
 float yawAngle;
 
+float joystickX;
+float joystickY;
+byte joystickButton;
+
 const byte octaveStep = 12;
 byte octave = 60;
 byte currentNote;
+//byte oldNote;
 
 MPU6050 mpu6050(Wire);
 
 void setup() {
   Serial.begin(9600);
-  //  Wire.begin();
-  //  mpu6050.begin();
-  //  mpu6050.calcGyroOffsets(true);
+  pinMode(joystickButtonPin, INPUT_PULLUP);
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
 }
 
 void loop() {
   checkFlexSensor();
   checkImu();
+  checkJoystick();
 }
 
 void checkFlexSensor() {
 
   value = analogRead(flexPin);
   value = map(value, 700, 900, 0, 500); //Map value 0-1023 to 0-255 (PWM)
- 
-  
-    if (value > 300 && cond==false) {
-  
-  
-      rollAccel = mpu6050.getAccX();
-      rollAngle = mpu6050.getAngleX();
-      rollGyro = mpu6050.getGyroX();
-  
-      byte rollValue = map(rollAngle, -40, 60, 0, 11);
-  
-      currentNote = (findClosestValue(majorScale, majorScaleLength, rollValue)+octave)%127;
-  
-//       Serial.println("Note on");
-//       Serial.println(currentNote);
-//  
-      noteOn(0x90, currentNote, 0x45);
-      delay(500);
-      cond = true;
-    }
-    else if (value <300 && cond==true) {
-      noteOn(0x80, currentNote, 0x45);
-      cond=false;
-      delay(500);
-//      Serial.println("note off");
-    }
+
+
+  if (value > 300 && cond == false) {
+
+
+    rollAccel = mpu6050.getAccX();
+    rollAngle = mpu6050.getAngleX();
+    rollGyro = mpu6050.getGyroX();
+
+    byte rollValue = map(rollAngle, -40, 60, 0, 11);
+
+    currentNote = (findClosestValue(majorScale, majorScaleLength, rollValue) + octave) % 127;
+
+    send_midi(NOTE_ON, currentNote, 0x45);
+    cond = true;
+
+
+  }
+  else if (value < 300 && cond == true) {
+    send_midi(NOTE_OFF, currentNote, 0x45);
+    cond = false;
+  }
 
 }
 
-int findClosestValue(byte arr[], byte n, byte target) {
+byte findClosestValue(byte arr[], byte n, byte target) {
   if (target <= arr[0])
     return arr[0];
   if (target >= arr[n - 1])
@@ -114,6 +120,41 @@ byte getClosest(byte val1, byte val2,
     return val1;
 }
 
+
+void checkJoystick() {
+
+  joystickX = analogRead(joystickXPin);
+  joystickY = analogRead(joystickYpin);
+  joystickButton = digitalRead(joystickButtonPin);
+
+  //Serial.print(joystickX);
+  //Serial.print(joystickY);
+  //Serial.println(joystickButton);
+
+  bool isJoystickPushed = false;
+
+  int pitchValue;
+
+  if (!digitalRead(joystickButtonPin)) {
+    isJoystickPushed = true;
+    pitchValue = mpu6050.getGyroZ();
+    pitchValue = map(pitchValue , 200, -200, 0x00, 0x60);
+
+    if (pitchValue > 0x00 && pitchValue < 0x60) {
+      Serial.write(0xE0);
+      Serial.write(0x00);
+      Serial.write(pitchValue);
+      delay(50);
+    }
+
+  }
+
+  if ( isJoystickPushed && digitalRead(joystickButtonPin)) {
+    send_midi(NOTE_OFF, currentNote, 0x45);
+  }
+
+}
+
 void checkImu() {
   mpu6050.update();
 
@@ -125,24 +166,21 @@ void checkImu() {
   yawGyro = mpu6050.getGyroZ();
   yawAngle = mpu6050.getAngleZ();
 
+
   if ( pitchGyro < -300) {
     //    Serial.println("Pitch Up");
     octave = octave + octaveStep;
-    delay(1000);
   }
   else if (pitchGyro > 300 ) {
     //    Serial.println("Pitch Down");
     octave = octave - octaveStep;
-    delay(1000);
   }
 
   if (yawGyro > 300) {
     //    Serial.println("Yaw Left");
-    delay(1000);
   }
   else if ( yawGyro < -300) {
     //    Serial.println("Yaw Right");
-    delay(1000);
   }
 
   //  if(roll > 1.80 && rollAngle > 45){
@@ -153,6 +191,22 @@ void checkImu() {
   //    Serial.println("Roll Left");
   //    delay(3000);
   //  }
+
+  //    Serial.write(0xB0);
+  //    Serial.write(0x00);
+  //    Serial.write(0x01);
+  //
+  //    Serial.write(0xB0);
+  //    Serial.write(0x20);
+  //    Serial.write(0x01);
+  //
+  //    Serial.write(INSTRUMENT );
+  //    Serial.write(index);
+
+
+  //    Serial.write(0xB0);
+  //    Serial.write(0x07); // VOLUME
+  //    Serial.write(index);
 
 
 }
